2d9eb6b97d7d0ea64538f779fbafd0456ba322e1
Tyson <tyson.foster@hotmail.com>
2021-12-07 21:23:09 -0800

    add test for database upload

--- test-contact.php
+++ test-contact.php
@@ -32,7 +32,7 @@ $goodMessage = "This is a valid message.";
 $goodEvent = "Commander";
 
 echo "<p>BELOW TEST MUST PASS FOR OTHER VALIDATION TESTS TO BE RELIABLE</p>";
-validationTest("good", true, $goodName, $goodEmail, $goodMessage, $goodEvent, $event_list);
+validationTest("good input", true, $goodName, $goodEmail, $goodMessage, $goodEvent, $event_list);
 echo "<p>ABOVE TEST MUST PASS FOR OTHER VALIDATION TESTS TO BE RELIABLE</p>";
 
 //bad input tests
@@ -40,19 +40,19 @@ echo "<p>ABOVE TEST MUST PASS FOR OTHER VALIDATION TESTS TO BE RELIABLE</p>";
 //missing values
 //submission must include name
 $badName = NULL;
-validationTest ("No Name", false, $badName, $goodEmail, $goodMessage, $goodEvent, $event_list);
+validationTest("No Name", false, $badName, $goodEmail, $goodMessage, $goodEvent, $event_list);
 
 //submission must include email
 $badEmail = NULL;
-validationTest ("No Email", false, $goodName, $badEmail, $goodMessage, $goodEvent, $event_list);
+validationTest("No Email", false, $goodName, $badEmail, $goodMessage, $goodEvent, $event_list);
 
 //missing message is ok
 $badMessage = NULL;
-validationTest ("No message", true, $goodName, $goodEmail, $badMessage, $goodEvent, $event_list);
+validationTest("No message", true, $goodName, $goodEmail, $badMessage, $goodEvent, $event_list);
 
 //NULL event should default to 'none' which is acceptable
 $badEvent = NULL;
-validationTest ("No Event", true, $goodName, $goodEmail, $goodMessage, $badEvent, $event_list);
+validationTest("No Event", true, $goodName, $goodEmail, $goodMessage, $badEvent, $event_list);
 
 //bad values
 
@@ -61,11 +61,11 @@ validationTest ("No Event", true, $goodName, $goodEmail, $goodMessage, $badEvent
 //email validation
 //not an email
 $badEmail = "NotAnEmail";
-validationTest ("Not Email", false, $goodName, $badEmail, $goodMessage, $goodEvent, $event_list);
+validationTest("Not Email", false, $goodName, $badEmail, $goodMessage, $goodEvent, $event_list);
 
 //multiple emails
 $badEmail = "test1@example.com test2@moreexamples.com";
-validationTest ("Multiple Emails", false, $goodName, $badEmail, $goodMessage, $goodEvent, $event_list);
+validationTest("Multiple Emails", false, $goodName, $badEmail, $goodMessage, $goodEvent, $event_list);
 
 //test for too long string
 $badMessage = "a";
@@ -73,14 +73,17 @@ $badMessage = "a";
 for ($x = 0; $x < 501; $x++) {
   $badMessage .= "a";
 }
-validationTest ("Message Too Long", false, $goodName, $goodEmail, $badMessage, $goodEvent, $event_list);
+validationTest("Message Too Long", false, $goodName, $goodEmail, $badMessage, $goodEvent, $event_list);
 
 //bad event
 $badEvent = "Not a real event";
-validationTest ("Bad Event", false, $goodName, $goodEmail, $goodMessage, $badEvent, $event_list);
+validationTest("Bad Event", false, $goodName, $goodEmail, $goodMessage, $badEvent, $event_list);
 
 echo "<p>VALIDATION TESTING COMPLETE</p>";
 
+//database upload testing
+//NOTE: uploadFormData returns 1 or -1 for true and false, so set $expected to those values
+uploadTest($tableName, "good input upload", 1, $goodName, $goodEmail, $goodMessage, $goodEvent);
 
 //delete test table when done
 $sql = "DROP TABLE $tableName";
@@ -97,12 +100,18 @@ function validationTest($test, $expected, $name, $email, $message, $event, $even
   $_POST['event'] = $event;
   //test the loaded input
   if (handleForm($event_list, true) == $expected) {
-    echo "<p>TEST FOR $test PASSED</p>";
+    echo "<p>TEST PASSED FOR $test</p>";
   } else {
-    echo "<p>TEST FOR $test FAILED</p>";
+    echo "<p>TEST FAILED FOR $test</p>";
   }
 }
 
-function uploadTest($tableNme, $test, $expected, $name, $email, $message, $event) {
-
+//function for testing that uploading to the database works as expected
+//NOTE: uploadFormData returns 1 or -1 for true and false, so set $expected to those values
+function uploadTest($tableName, $test, $expected, $name, $email, $message, $event) {
+  if (uploadFormData($tableName, $name, $email, $message, $event) == $expected) {
+    echo "<p>TEST PASSED FOR $test</p>";
+  } else {
+    echo "<p>TEST FAILED FOR $test</p>";
+  }
 }
